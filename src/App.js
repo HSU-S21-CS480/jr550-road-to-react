32c1d38c5281fbf1015498cf17a7681e04308a2e
jritter77 <jr550@humboldt.edu>
2021-02-02 15:44:42 -0800

    after "css"

--- src/App.js
+++ src/App.js
@@ -135,18 +135,16 @@ const App = () => {
 
   //Return the HTML element which contains our webpage
   return (
-    <div className="App">
-      <h1>My Hacker Stories</h1>
+    <div className="container">
+      <h1 className="headline-primary">My Hacker Stories</h1>
 
       
       <SearchForm
         searchTerm={searchTerm}
         onSearchInput={handleSearchInput}
         onSearchSubmit={handleSearchSubmit}
+        className="button_small"
       />
-      
-
-      <hr />
 
 
       {stories.isError && <p>Something went wrong...</p>}
@@ -165,9 +163,10 @@ const App = () => {
 const SearchForm = ({
   searchTerm,
   onSearchInput,
-  onSearchSubmit
+  onSearchSubmit,
+  className
 }) => (
-  <form onSubmit={onSearchSubmit}>
+  <form onSubmit={onSearchSubmit} className="search-form">
         <InputWithLabel
           id="search"
           value={searchTerm}
@@ -177,7 +176,7 @@ const SearchForm = ({
           <strong>Search:</strong>
         </InputWithLabel>
 
-        <button type="submit" disabled={!searchTerm}>
+        <button type="submit" disabled={!searchTerm} className={className}>
           Submit
         </button>
       </form>
@@ -197,7 +196,9 @@ const InputWithLabel = ({id, value, type = 'text', onInputChange, isFocused, chi
 
   return (
   <>
-    <label htmlFor={id}>{children}</label>
+    <label htmlFor={id} className="label">
+      {children}
+    </label>
     &nbsp;
     
     <input
@@ -205,7 +206,9 @@ const InputWithLabel = ({id, value, type = 'text', onInputChange, isFocused, chi
       type={type}
       value={value} 
       autoFocus={isFocused}
-      onChange={onInputChange} />
+      onChange={onInputChange}
+      className="input" 
+    />
   </>
   );
 };
@@ -226,15 +229,15 @@ list.map(item => (
 
 const Item = ({ item, onRemoveItem }) => (
   
-    <div>
-      <span>
+    <div className="item">
+      <span style={{ width: '40%' }}>
         <a href={item.url}>{item.title}</a>
       </span>
-      <span>{item.author}</span>
-      <span>{item.num_comments}</span>
-      <span>{item.points}</span>
-      <span>
-        <button type="button" onClick={() => onRemoveItem(item)}>
+      <span style={{ width: '30%' }}>{item.author}</span>
+      <span style={{ width: '10%' }}>{item.num_comments}</span>
+      <span style={{ width: '10%' }}>{item.points}</span>
+      <span style={{ width: '10%' }}>
+        <button type="button" onClick={() => onRemoveItem(item)} className="button_small">
           Dismiss
         </button>
       </span>
